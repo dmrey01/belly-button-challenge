c7cd1800c996a0cd115219244468e4e07904c848
First user <140979441+dmrey01@users.noreply.github.com>
2024-03-09 18:46:52 -0600

    updated files

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,70 +1,59 @@
-//set up URL "
-const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
+//set up URL 
+const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
-// Promise Pending
-const dataPromise = d3.json(url);
-console.log("Data Promise:", dataPromise);
-
-//Fetch the Json data and console log it
+// Fetch the JSON data and console log it
 d3.json(url).then(function (data) {
     console.log(data);
-    
     init(data);
 });
 
-function init() {
-    d3.json(url).then(function (data) {
-        console.log(data);
-        
-        let dropdown = d3.select("#selDataset");
-        
-        let sampleIDs = data.names
-        console.log(sampleIDs);
-        sampleIDs.forEach(function (id) {
-            dropdown.append("option").text(id).property("value", id);
-        });
+function init(data) {
+    let dropdown = d3.select("#selDataset");
+    let sampleIDs = data.names;
+    sampleIDs.forEach(function (id) {
+        dropdown.append("option").text(id).property("value", id);
+    });
 
-        let selection = sampleIDs[0];
-        barChart(selection, data);
-        metaData(selection, data.metadata);
-        optionChanged(selection, data.samples, data.metadata);
-    })
-}   
-
-function optionChanged(value) {
-    console.log(value);
-    barChart(value);
-    metaData(value);
+    let selection = sampleIDs[0];
+    updateCharts(selection, data);
 }
 
-function metaData(demographicInfo) {
+function optionChanged() {
+    let dropdownMenu = d3.select("#selDataset");
+    let selection = dropdownMenu.property("value");
     d3.json(url).then(function (data) {
-        console.log(metaData);
-        let demographicInfoDiv = d3.select("#sample-metadata");
-        demographicInfoDiv.html("");
-        for (const [key, value] of Object.entries(demographicInfo)) {
-            demographicInfoDiv.append("p").text(`${key}: ${value}`);
-        }
+        updateCharts(selection, data);
     });
 }
 
+function updateCharts(selection, data) {
+    barChart(selection, data);
+    metaData(selection, data.metadata);
+    bubbleChart(selection, data);
+}
+
+function metaData(selection, metadata) {
+    let demographicInfoDiv = d3.select("#sample-metadata");
+    demographicInfoDiv.html("");
+    let selectedMetadata = metadata.find(item => item.id == selection);
+    for (const [key, value] of Object.entries(selectedMetadata)) {
+        demographicInfoDiv.append("p").text(`${key}: ${value}`);
+    }
+}
+
 function barChart(selection, data) {
-    d3.json(url).then(function (data) {
-        console.log(data);
     let sampleDataInfo = data.samples.find(sample => sample.id === selection);
-    let sample_value = sampleDataInfo.sample_values.slice(0, 10);
+    let sample_values = sampleDataInfo.sample_values.slice(0, 10);
     let otu_ids = sampleDataInfo.otu_ids.slice(0, 10);
     let otu_labels = sampleDataInfo.otu_labels.slice(0, 10);
 
-
     let trace1 = [{
-        x: sample_value.reverse(),
+        x: sample_values.reverse(),
         y: otu_ids.map(item => `OTU ${item}`).reverse(),
         text: otu_labels.reverse(),
         type: 'bar',
         orientation: 'h'
     }];
-
     let layout = {
         title: 'Bacteria Count ',
         xaxis: { title: 'Sample Values' },
@@ -72,23 +61,35 @@ function barChart(selection, data) {
         width: 600,
         height: 400
     };
-
     Plotly.newPlot('bar', trace1, layout);
-    
-    });
-};
+}
 
 //bubble chart
-//function bubbleChart(selection, data) {
-    //d3.json(url).then(function (data) {
-        //console.log("Generating bar chart...");
-        //let sampleDataInfo = data.samples.find(sample => sample.id === selection);
-       // let sample_value = sampleDataInfo.sample_values.slice(0, 10);
-       // let otu_ids = sampleDataInfo.otu_ids.slice(0, 10);
-       // let otu_labels = sampleDataInfo.otu_labels.slice(0, 10);
+function bubbleChart(selection, data) {
+    let sampleDataInfo = data.samples.find(sample => sample.id === selection);
+    let sample_values = sampleDataInfo.sample_values;
+    let otu_ids = sampleDataInfo.otu_ids;
+    let otu_labels = sampleDataInfo.otu_labels;
+    let maxMarkerSize = Math.max(...sample_values);
+    let trace2 = {
+        x: otu_ids,
+        y: sample_values,
+        mode: 'markers',
+        marker: {
+            size: sample_values.map(value => value * .25),
+            sizeref: 1,
+            color: otu_ids,
+            text: otu_labels,
+        }
+    };
+    layout2 = {
+        title: "Bacteria Count",
+        showlegend: false,
+        height: 600,
+        width: 600,
+        xaxis: { title: `OTU ID` },
+    };
+    Plotly.newPlot('bubble', [trace2], layout2);
+}
+
 init();
-
-
-
-
-
